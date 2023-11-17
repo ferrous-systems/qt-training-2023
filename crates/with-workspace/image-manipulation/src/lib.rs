9b49732274408f25c49e8ad4375d6e22cce904a0
Leon Matthes <leon.matthes@kdab.com>
2023-11-17 14:09:27 +0100

    Fix read_cursor -> cursor rename

--- crates/with-workspace/image-manipulation/src/lib.rs
+++ crates/with-workspace/image-manipulation/src/lib.rs
@@ -2,12 +2,12 @@ use std::io::Cursor;
 
 use rustagram::image::io::Reader;
 use rustagram::image::ImageOutputFormat;
-use rustagram::{RustagramFilter, FilterType};
+use rustagram::{FilterType, RustagramFilter};
 
 pub fn apply_filter(img: &[u8], filter: FilterType) -> Vec<u8> {
     log::debug!("image: {} bytes, filter: {:?}", img.len(), filter);
 
-    let read_cursor = Cursor::new(img);
+    let cursor = Cursor::new(img);
     let img = Reader::new(cursor)
         .with_guessed_format()
         .unwrap()
@@ -15,7 +15,7 @@ pub fn apply_filter(img: &[u8], filter: FilterType) -> Vec<u8> {
         .unwrap();
 
     let out = img.to_rgba8().apply_filter(filter);
-    
+
     let mut bytes: Vec<u8> = Vec::new();
     let mut write_cursor = Cursor::new(&mut bytes);
     out.write_to(&mut write_cursor, ImageOutputFormat::Png)
@@ -23,3 +23,4 @@ pub fn apply_filter(img: &[u8], filter: FilterType) -> Vec<u8> {
 
     bytes
 }
+
